362005165d099f3df70d60b00e898543de6a528b
soniya.shinde <soniya.shinde@sunbeaminfo.com>
2020-02-26 11:25:21 +0530

    save updates

--- file_handling/Student_hackathon/src/Student_hackathon.c
+++ file_handling/Student_hackathon/src/Student_hackathon.c
@@ -15,8 +15,8 @@
 struct Customer
 {
 	int id;
-	char *name;
-	char *address;
+	char name[30];
+	char address[30];
 };
 typedef struct Product
 {
@@ -33,7 +33,7 @@ struct Date
 struct Order
 {
 	int id,customer_id,product_id,quantity;
-	char *status;
+	char status[20];
 	struct Date order_date;
 };
 void Register_customer()
@@ -146,13 +146,13 @@ void Add_Product()
 	}
 	else
 	{
-		printf("\nEnter id");
+		printf("\nEnter id :");
 		scanf("%d",&p.id);
-		printf("\nEnter Name");
+		printf("\nEnter Name :");
 		scanf("%s",&p.name);
-		printf("\nEnter Category");
+		printf("\nEnter Category :");
 		scanf("%s",&p.category);
-		printf("\nEnter Rate");
+		printf("\nEnter Rate :");
 		scanf("%f",&p.rate);
 		n=fwrite(&p,sizeof(p),1,fp);
 		if(n>0)
@@ -182,10 +182,10 @@ void List_Products()
 	}
 	else
 	{
-		printf("\nID \t Name \t\t Category \t\t Rate");
+		printf("\nID \t Name \t   Category \t Rate\n");
 		while(fread(&p,sizeof(p),1,fp)== 1)
 		{
-			printf("\n%d\t %s \t\t %s \t\t %f",p.id,&p.name,&p.category,p.rate);
+			printf("\n%-7d %-10s  %-10s  %f",p.id,&p.name,&p.category,p.rate);
 
 		}
 	}
@@ -229,14 +229,8 @@ void Find_Product_by_name()
 
 int comp_product_By_name(const void *p1, const void *p2)
 {
-	printf("hello");
-//	struct Product *p=(struct Product*)p1;
-//	struct Product *p4=(struct Product*)p2;
 
-//	Product_t *p=(Product_t *)p1;
-//	Product_t *p4=(Product_t *)p2;
-
-	struct Product *p=(struct Product*)p1, *p4=(struct Product*)p2;
+    struct Product *p=(struct Product*)p1, *p4=(struct Product*)p2;
 	int diff=strcmp((char *)p->name, (char *)p4->name);
 	return diff;
 }
@@ -244,8 +238,8 @@ void sort_Product_By_Name()
 {
 	FILE *fp;
 	int n, i = 0;
-	//struct Product arr[10];
-	//struct Product p;
+	char category[20];
+
 	Product_t arr[10];
 	Product_t  p;
 	fp=fopen("product.db","rb");
@@ -256,27 +250,32 @@ void sort_Product_By_Name()
 	}
 	else
 	{
+		printf("\nEnter Category :");
+		scanf("%s",&category);
 		while(fread(&p, sizeof(Product_t), 1, fp) == 1)
 		{
-			arr[i]=p;
-			i++;
+			if(strcmp(p.category,category)==0)
+			{
+			  arr[i]=p;
+			  i++;
+			}
 		}
 
 
 		qsort(arr,i,sizeof(Product_t),comp_product_By_name);
 		printf("\n List Of product sorted by Name\n");
-		printf("\nId \t Name \t Category \t Price\n");
+		printf("\nID \t Name \t   Category \t Rate\n");
 		for(int j=0;j<i;j++)
 		{
-			printf("\n %d \t %s \t %s \t %f",arr[j].id,&arr[j].name,&arr[j].category,arr[j].rate);
+			printf("\n%-7d %-10s  %-10s  %f",arr[j].id,&arr[j].name,&arr[j].category,arr[j].rate);
 		}
-
+		printf("\n");
 	}
 }
 
 int comp_product_By_price(const void *e1, const void *e2)
 {
-	printf("hello");
+
 	struct Product *p1=(struct Product*)e1, *p2=(struct Product*)e2;
 	double diff = p1->rate - p2->rate;
 	if(diff > 0)
@@ -292,6 +291,7 @@ void sort_Product_By_Price()
 	int i=0,n;
 	struct Product arr[10];
 	struct Product p;
+	char category[20];
 	fp=fopen("product.db","rb");
 	if(fp == NULL)
 	{
@@ -300,22 +300,383 @@ void sort_Product_By_Price()
 	}
 	else
 	{
+		printf("\nEnter Category :");
+		scanf("%s",&category);
 		while(fread(&p,sizeof(p),1,fp)==1)
 		{
-			arr[i++]=p;
+			if(strcmp(p.category,category)==0)
+			{
+			  arr[i]=p;
+			  i++;
+			}
 		}
 		qsort(arr,i,sizeof(struct Product),comp_product_By_price);
 		printf("\nList of Product ascending order of price\n");
-		printf("\nId \t Name \t Category \t Price\n");
+		printf("\nID \t Name \t   Category \t Rate\n");
 		for(int j=0;j<i;j++)
 		{
-			printf("\n %d \t %s \t %s \t %f",arr[j].id,&arr[j].name,&arr[j].category,arr[j].rate);
+			printf("\n%-7d %-10s  %-10s  %f",arr[j].id,&arr[j].name,&arr[j].category,arr[j].rate);
 		}
+		printf("\n");
 
 	}
 }
 
+void order_item()
+{
+	struct Order o;
+	FILE *fp;
+	fp=fopen("order.db","ab");
+	if(fp==NULL)
+	{
+		perror("\n Failed to open...");
+		return;
+	}
+	else
+	{
+		printf("\nEnter Order Details ....\n");
+		printf("\nEnter order ID :");
+		scanf("%d",&o.id);
+		printf("\nEnter Customer ID :");
+		scanf("%d",&o.customer_id);
+		printf("\nEnter Product ID :");
+		scanf("%d",&o.product_id);
+		printf("\nEnter Quantity :");
+		scanf("%d",&o.quantity);
+		printf("\nEnter order status (pending):");
+		scanf("%s",&o.status);
+		printf("\nEnter order Date (dd/MM/yy) :");
+		scanf("%d/%d/%d",&o.order_date.day,&o.order_date.month,&o.order_date.year);
+		int n=fwrite(&o,sizeof(o),1,fp);
+		if(n>0)
+		{
+			printf("\nOrder placed Successfully...");
 
+		}
+		else
+		{
+			printf("\nFailed to placed order...");
+		}
+
+	}
+	fclose(fp);
+
+}
+void pending_order(int custid,char *cname)
+{
+	FILE *fp,*fs;
+	int n;
+	struct Order o;
+	struct Product p;
+	int flag=0;
+	char status[20]="pending";
+	fp=fopen("order.db","rb");
+	fs=fopen("product.db","rb");
+	if(fp==NULL && fs==NULL)
+	{
+		perror("\n Failed to open...");
+		return;
+	}
+	else
+	{
+		printf("\n Welcome %s\n",cname);
+		printf("\n List of Pending Orders...");
+		printf("\nOrderID \t Name \t Qty\n");
+		while(fread(&o,sizeof(o),1,fp)==1)
+		{
+			if(strcmp(o.status,status)==0 && o.customer_id==custid)
+			{
+				flag=1;
+				while(fread(&p,sizeof(p),1,fs)==1)
+				{
+				   if(p.id==o.product_id)
+				   {
+					   printf("\n %-5d \t\t %-10s %-5d",o.id,p.name,o.quantity);
+				   }
+				}
+
+			}
+		}
+	}
+    if(flag==0)
+    {
+    	printf("\nNo pending Orders...\n");
+    }
+  fclose(fp);
+
+}
+
+void delivered_order(int custid,char *cname)
+{
+		FILE *fp,*fs;
+		int n;
+		struct Order o;
+		struct Product p;
+		int flag=0;
+		char status[20]="delivered";
+		fp=fopen("order.db","rb");
+		fs=fopen("product.db","rb");
+		if(fp==NULL && fs==NULL)
+		{
+			perror("\n Failed to open...");
+			return;
+		}
+		else
+		{
+			printf("\n Welcome %s\n",cname);
+			printf("\n List of Delivered Orders...");
+			printf("\nOrderID \t Product ID \t Qty\n");
+			while(fread(&o,sizeof(o),1,fp)!=0)
+					{
+				       // printf("\nwhile...");
+						if(strcmp(o.status,status)==0 && o.customer_id==custid)
+						{
+							// printf("\nif...");
+							   printf("\n %-5d \t\t %-10d %-5d",o.id,o.product_id,o.quantity);
+							 flag=1;
+//							while(fread(&p,sizeof(p),1,fs)==1)
+//							{
+//								printf("\np.while..");
+//								printf("o:%d p:%d",o.product_id,p.id);
+//							   if(p.id==o.product_id)
+//							   {
+//								   printf("\np while...");
+//								   printf("\n %-5d \t\t %-10s %-5d",o.id,p.name,o.quantity);
+//							   }
+//							}
+
+						}
+					}
+		}
+	    if(flag==0)
+	    {
+	    	printf("\nNo delivered Orders...\n");
+	    }
+      fclose(fp);
+
+}
+void List_Pending_Orders()
+{
+	    FILE *fp;
+		int n;
+		struct Order o;
+
+		int flag=0;
+		char status[20]="pending";
+		fp=fopen("order.db","rb");
+
+		if(fp==NULL)
+		{
+			perror("\n Failed to open...");
+			return;
+		}
+		else
+		{
+
+			printf("\n List of Pending Orders...\n");
+			printf("\nOrderID \t ProductID\t Qty \t CustomerID \t Status \t OrderDate\n");
+			while(fread(&o,sizeof(o),1,fp)==1)
+			{
+				if(strcmp(o.status,status)==0)
+				{
+					flag=1;
+					printf("\n%-5d \t\t %-5d \t \t%d \t %d \t\t %-10s \t %d/%d/%d",o.id,o.product_id,o.quantity,o.customer_id,&o.status,o.order_date.day,o.order_date.month,o.order_date.year);
+
+
+
+				}
+			}
+		}
+	    if(flag==0)
+	    {
+	    	printf("\nNo pending Orders...\n");
+	    }
+
+
+}
+void List_delivered_Orders()
+{
+	  FILE *fp;
+			int n;
+			struct Order o;
+
+			int flag=0;
+			char status[20]="delivered";
+			fp=fopen("order.db","rb");
+
+			if(fp==NULL)
+			{
+				perror("\n Failed to open...");
+				return;
+			}
+			else
+			{
+
+				printf("\n List of Pending Orders...\n");
+				printf("\nOrderID \t ProductID\t Qty \t CustomerID \t Status \t OrderDate\n");
+				while(fread(&o,sizeof(o),1,fp)==1)
+				{
+					if(strcmp(o.status,status)==0)
+					{
+						flag=1;
+						printf("\n%-5d \t\t %-5d \t \t%d \t %d \t\t %-10s \t %d/%d/%d",o.id,o.product_id,o.quantity,o.customer_id,&o.status,o.order_date.day,o.order_date.month,o.order_date.year);
+
+
+
+					}
+				}
+			}
+		    if(flag==0)
+		    {
+		    	printf("\nNo delivered Orders...\n");
+		    }
+
+}
+void Delete_Product()
+{
+
+		FILE *fs, *fd;
+		struct Product p;
+		int id, found=0;
+		printf("\nEnter  Product id to delete: ");
+		scanf("%d", &id);
+		fs = fopen("product.db", "rb");
+		if(fs == NULL)
+		{
+			perror("\nfopen() failed");
+			return;
+		}
+		fd = fopen("temp.db", "ab");
+		if(fd == NULL)
+		{
+			perror("\nfopen() failed");
+			return;
+		}
+		while(fread(&p, sizeof(p), 1, fs) != 0)
+		{
+			if(p.id == id)
+				found = 1;
+			else
+				fwrite(&p, sizeof(p), 1, fd);
+		}
+		fclose(fd);
+		fclose(fs);
+		remove("product.db");
+		rename("temp.db", "product.db");
+
+		if(found == 0)
+			printf("\nNo such product found...\n");
+		else
+		{
+			printf("\n Product Deleted successfully..\n");
+		}
+
+
+
+}
+
+void Edit_Product()
+{
+	FILE *fp;
+	int n,productId;
+	struct Product p;
+	int flag=0;
+	long size=sizeof(struct Product);
+	fp=fopen("product.db","rb+");
+	if(fp == NULL)
+	{
+		perror("\nFailed to open..");
+		return;
+	}
+	else
+	{
+		printf("\n Enter Product ID to Find :");
+		scanf("%d",&productId);
+		while(fread(&p,sizeof(p),1,fp)!=0)
+		{
+			if(p.id==productId)
+			{
+				flag=1;
+				printf("\n Old Record...");
+				printf("\nId \t Name \t Category \t Rate");
+				printf("\n%d\t %s \t %s \t %f",p.id,&p.name,&p.category,p.rate);
+				printf("\nEnter id :");
+				scanf("%d",&p.id);
+				printf("\nEnter Name :");
+				scanf("%s",&p.name);
+				printf("\nEnter Category :");
+				scanf("%s",&p.category);
+				printf("\nEnter Rate :");
+				scanf("%f",&p.rate);
+				fseek(fp, -size, SEEK_CUR);
+				n=fwrite(&p,sizeof(p),1,fp);
+				if(n>0)
+				printf("\nProduct update successfully..");
+				else
+				{
+					printf("\nFail to update..");
+				}
+
+			}
+
+		}
+	}
+
+	if(!flag)
+	{
+		printf("\nNo Product Find...\n");
+	}
+	fclose(fp);
+
+}
+void Dispatch_order()
+{
+	    FILE *fp;
+		int n,orderId;
+		struct Order o;
+		int flag=0;
+		char status[20]="pending";
+		long size=sizeof(struct Order);
+		fp=fopen("order.db","rb+");
+		if(fp == NULL)
+		{
+			perror("\nFailed to open..");
+			return;
+		}
+		else
+		{
+			printf("\n Enter Order ID to Find :");
+			scanf("%d",&orderId);
+			while(fread(&o,sizeof(o),1,fp)!=0)
+			{
+				if(o.id==orderId && strcmp(o.status,status)==0)
+				{
+					flag=1;
+
+					printf("\nEnter Status :");
+					scanf("%s",&o.status);
+
+					fseek(fp, -size, SEEK_CUR);
+					n=fwrite(&o,sizeof(o),1,fp);
+					if(n>0)
+					printf("\nOrder status update successfully..");
+					else
+					{
+						printf("\nFail to update..");
+					}
+
+				}
+
+			}
+		}
+
+		if(!flag)
+		{
+			printf("\nNo pending order Find...\n");
+		}
+		fclose(fp);
+
+}
 int main(void)
 {
 	int choice;
@@ -326,6 +687,7 @@ int main(void)
 		switch(choice)
 		{
 		case 0:
+			printf("\n Logout.....");
 			exit(0);
 			break;
 		case 1:
@@ -347,46 +709,72 @@ int main(void)
 
 void Customer(void)
 {
-	int choice;
-	do
+	int flag=0;
+	FILE *fp;
+	struct Customer c;
+	int n,custid;
+	fp=fopen("customers.db","rb");
+	if(fp == NULL)
 	{
-		printf("\n0.Exit\n"
-				"1.List Product categories\n"
-				"2.Product items per category(sorted by name)\n"
-				"3.Product items per category(sorted by price)\n"
-				"4.Order an item\n"
-				"5.List pending orders\n"
-				"6.List delivered orders\n");
-		printf("\nEnter your choice:");
-		scanf("%d",&choice);
-		switch(choice)
+		perror("\n Failed to open file");
+		exit(0);
+	}
+	else
+	{
+		printf("\nEnter Customer Id:");
+		scanf("%d",&custid);
+
+		while(fread(&c,sizeof(c),1,fp)==1)
 		{
-		case 0:
-			exit(0);
-			break;
-		case 1:
-			List_Product_categories();
-			break;
-		case 2:
-			sort_Product_By_Name();
-			break;
-		case 3:
-			sort_Product_By_Price();
-			break;
-		case 4:
-			//order_item();
-			break;
-		case 5:
-			//pending_order();
-			break;
-		case 6:
-			//delivered_order();
-			break;
-		default:
-			printf("\n Invalid choice....");
-			break;
+			if(c.id==custid)
+			{
+				flag=1;
+			int choice;
+			do
+			{
+				printf("\n0.Exit\n"
+						"1.List Product categories\n"
+						"2.Product items per category(sorted by name)\n"
+						"3.Product items per category(sorted by price)\n"
+						"4.Order an item\n"
+						"5.List pending orders\n"
+						"6.List delivered orders\n");
+				printf("\nEnter your choice:");
+				scanf("%d",&choice);
+				switch(choice)
+				{
+				case 0:
+				    return;
+				case 1:
+					List_Product_categories();
+					break;
+				case 2:
+					sort_Product_By_Name();
+					break;
+				case 3:
+					sort_Product_By_Price();
+					break;
+				case 4:
+					order_item();
+					break;
+				case 5:
+					pending_order(custid,c.name);
+					break;
+				case 6:
+					delivered_order(custid,c.name);
+					break;
+				default:
+					printf("\n Invalid choice....");
+					break;
+				}
+			}while(choice!=0);
+		 }
 		}
-	}while(choice!=0);
+    }
+	if(flag==0)
+	{
+		printf("\n No customer Record Found...");
+	}
 
 }
 int sub_menu_admin()
@@ -433,16 +821,19 @@ void Admin(void)
 			Find_Product_by_name();
 			break;
 		case 5:
-			//Edit_Product();
+			Edit_Product();
 			break;
 		case 6:
-			//Delete_Product();
+			Delete_Product();
 			break;
 		case 7:
-			//List_Pending_Orders();
+			List_Pending_Orders();
 			break;
 		case 8:
-			//List_delivered_Orders();
+			List_delivered_Orders();
+			break;
+		case 9:
+			Dispatch_order();
 			break;
 		case 10:
 			Add_Product();
